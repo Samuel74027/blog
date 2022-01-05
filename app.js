d2d822923a0d7f9ead4b07341128be40cfeee2e4
Samuel74028 <samuel.yenlin@gmail.com>
2022-01-04 16:13:53 -0800

    practice

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const app = express();
 app.set("view engine", "ejs");
 app.listen(3000);
 
+let blogs = [
+  { title: "First blog", content: "fsdnfjksdfnjsnfjnsjnfsjanf" },
+  { title: "Second blog", content: "fsdnfjksdfnjsnfjnsjnfsjanf" },
+  { title: "Third blog", content: "fsdnfjksdfnjsnfjnsjnfsjanf" },
+];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
+
+app.use((req, res) => {
+  res.render("404", { title: "404" });
 });
